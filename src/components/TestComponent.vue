a7f7214c0816ae3b00f54ad9788d8b0bd785489a
Keith Jones <keithjones@Keiths-MacBook-Pro.local>
2018-09-17 13:06:02 -0600

    created TestComponent

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,26 @@
+// example of reactivity
+
+<template>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            a: 1,
+            b: 2,
+            c: a + b
+        }
+    },
+    computed: {
+        c() {
+            return this.a +this.b
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
